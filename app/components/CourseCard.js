50adcb14ecfdcfac027e9f6b269c3ba99a917828
karasi-sonica <karasisonica@gmail.com>
2025-11-03 05:15:41 +0530

    added components

--- app/components/CourseCard.js
+++
@@ -1,8 +0,0 @@
-export default function CourseCard({ title, instructor }) {
-  return (
-    <div className="course-card">
-      <h3>{title}</h3>
-      <p>Instructor: {instructor}</p>
-    </div>
-  );
-}
--- app/components/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p>© 2025 LMS Project. All Rights Reserved.</p>
-    </footer>
-  );
-}
